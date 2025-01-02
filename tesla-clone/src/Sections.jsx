a801b91cc3c6a7db89beb9357d122d8ee87d616f
Mário Costa <Gearoftech2023@gmail.com>
2025-01-02 09:56:30 -0300

    The Sections component was finally imported

---
+++ tesla-clone/src/Sections.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './SectionsImages.css';
+
+function Sections(){
+    return(
+        <>
+        <div className="Telsa-Home">
+            <img src='/assets/twoModels.jpg'></img>
+            <h1>Welcome</h1>
+        </div>
+        
+        </>
+    );
+}
+
+export default Sections;
+
+/////////////////////////////////////////////////////////////////////
--- tesla-clone/src/main.jsx
+++ tesla-clone/src/main.jsx
@@ -2,10 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Sections from './Sections.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Sections />
   </React.StrictMode>,
 );
