370616ad8306815db4a77ae614b606e2fd2b5f3d
Mário Costa <Gearoftech2023@gmail.com>
2025-01-03 11:47:18 -0300

    changes in the imagesBackground
    Still some bugs to solve

--- tesla-clone/src/Sections.jsx
+++ tesla-clone/src/Sections.jsx
@@ -4,40 +4,41 @@ import './SectionsImages.css';
 function Sections(){
     return(
         <>
-        <div className="Telsa-Home">
-            <img src="/src/assets/twoModels.jpg" id="twoModel" className="background-photo"></img>
+        <div id="Telsa-Home">
+           {/* <img src="/src/assets/twoModels.jpg" id="twoModel" className="background-photo"></img>  */}
             <h2 className="SubTitle">$299/mo Leasing</h2>
-            <div class="button-container">
-                <a class="styled-button">Order Model 3</a>
-                <a class="styled-button">Order Model Y</a>
+            <div className="button-container">
+                <a className="styled-button">Order Model 3</a>
+                <a className="styled-button">Order Model Y</a>
             </div>
         </div>
-        <div className="car-model-y">
-            <img src="/src/assets/ModelY_58.jpg" id="ModelY" className="background-photo"></img>
+        <div id="car-model-y">
+            {/* <img src="/src/assets/ModelY_58.jpg" id="ModelY" className="background-photo"></img> */}
         </div>
-        <div className="car-model-3">
-            <img src="/src/assets/Model3-66.jpg" id="Model3" className="background-photo"></img>
+        <div id="car-model-3">
+            {/* <img src="/src/assets/Model3-66.jpg" id="Model3" className="background-photo"></img> */}
         </div>
-        <div className="car-model-x">
-            <img src="/src/assets/ModelX_62.jpg" id="ModelX" className="background-photo"></img>
+        <div id="car-model-x">
+            {/* <img src="/src/assets/ModelX_62.jpg" id="ModelX" className="background-photo"></img> */}
         </div>
-        <div className="car-model-y">
-            <img src="/src/assets/ModelS_70.jpg" id="ModelS" className="background-photo"></img>
+        <div id="car-model-s">
+            {/* <img src="/src/assets/ModelS_70.jpg" id="ModelS" className="background-photo"></img> */}
         </div>
-        <div className="car-model-cbertruck">
-            <img src="/src/assets/Cybertruck_66.jpg" id="Cybertruck" className="background-photo"></img>
+        <div id="car-model-cbertruck">
+            <h2>This is the Cbertruck</h2>
+            {/* <img src="/src/assets/Cybertruck_66.jpg" id="Cybertruck" className="background-photo"></img> */}
         </div>
-        <div className="Solar-Panels">
-            <img src="/src/assets/SolarPanels_90.jpg" id="Panels" className="background-photo"></img>
+        <div id="Solar-Panels">
+            {/* <img src="/src/assets/SolarPanels_90.jpg" id="Panels" className="background-photo"></img> */}
         </div>
-        <div className="Solar-Roof">
-            <img src="/src/assets/SolarRoof_89.jpg" id="Roof" className="background-photo"></img>
+        <div id="Solar-Roof">
+            {/* <img src="/src/assets/SolarRoof_89.jpg" id="Roof" className="background-photo"></img> */}
         </div> 
-        <div className="Powerwall">
-            <img src="/src/assets/0x0-PowerwallPlus_01.jpg" id="Power" className="background-photo"></img>
+        <div id="Powerwall">
+            {/* <img src="/src/assets/0x0-PowerwallPlus_01.jpg" id="Power" className="background-photo"></img> */}
         </div> 
-        <div className="Acessories">
-            <img src="/src/assets/acessories.jpg" id="Acesso" className="background-photo"></img>
+        <div id="Acessories">
+            {/* <img src="/src/assets/acessories.jpg" id="Acesso" className="background-photo"></img> */}
         </div>    
         </>
     );
